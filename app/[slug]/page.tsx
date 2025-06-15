f71d587faf6947214fd1045fa226f644e1f0e05c
pix0010 <pix0010@gmail.com>
2025-06-15 17:05:39 +0200

    Фикс типов params и страницы демо2

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -21,7 +21,7 @@ export default async function DemoPage({ params }: Props) {
 
   try {
     data = (await import(`../../data/${params.slug}.json`)).default;
-  } catch (err) {
+} catch {
     return notFound();
   }
 
